b6145d91aad6d2dc0d6c4e397dcfb6bc7fb0381c
Virtual <officialimvirtual@gmail.com>
2023-10-05 17:21:47 +0200

    add platform support for linux & mac

--- src/util/extract.js
+++ src/util/extract.js
@@ -3,9 +3,18 @@ const path = require("path");
 
 const execFile = util.promisify(require("child_process").execFile);
 
+function getBinaryExtension() {
+	if (os.platform() === "win32") {
+		return ".exe";
+	}
+
+	return "";
+}
+
 module.exports = async function (folder) {
+	const os = require("os").platform()
 	const result = await execFile(
-		path.join(__dirname, "../../bin/moonwave-extractor"),
+		path.join(__dirname, `../../bin/${os}-moonwave-extractor${getBinaryExtension()}`),
 		["extract", folder]
 	);
 
