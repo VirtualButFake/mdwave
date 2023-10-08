80466bdb4d60ee95dc9e63c83a96d12e5ae3a5d7
Virtual <officialimvirtual@gmail.com>
2023-10-08 21:46:11 +0200

    Add docs and edit readme

--- docSources/f4c70e11df1daac2239717bde58ea6f003a9dc77/.vitepress/theme/components/Lua/CustomCodeBlock.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="language-lua vp-adaptive-theme">
-        <button class="copy" title="Copy Code" />
-        <span class="lang">lua</span>
-        <pre class="shiki github-dark vp-code-dark"><slot /></pre>
-        <pre class="shiki github-dark vp-code-light"><slot /></pre>
-    </div>
-</template>
